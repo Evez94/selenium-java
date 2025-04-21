3dbfe16a2a282e54cf338c4df82fa719507b751f
Evez Aslanov Rustem oglu <EvezA@emdkdedrx.gov.az>
2025-04-22 02:43:08 +0400

    Formpage change method

--- src/main/java/com/demoqa/pages/forms/FormsPage.java
+++ src/main/java/com/demoqa/pages/forms/FormsPage.java
@@ -10,7 +10,7 @@ public class FormsPage extends HomePage {
 
     private By practiceFormMenuItem = By.xpath("//li[@id='item-0']/span[text()='Practice Form']");
 
-
+//
     public PracticeFormPage clickPracticeForm(){
         scrollToElementJS(practiceFormMenuItem);
         click(practiceFormMenuItem);
