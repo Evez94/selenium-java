83eb01a30243bee4816528e29322dfa1d6bd7fea
Evez Aslanov <evez.aslanov.94@gmail.com>
2025-05-07 18:31:08 +0400

    Ilk commit

--- src/main/java/com/demoqa/pages/elements/WebTablesPage.java
+++ src/main/java/com/demoqa/pages/elements/WebTablesPage.java
@@ -1,4 +1,23 @@
 package com.demoqa.pages.elements;
 
-public class WebTablesPage {
+import org.openqa.selenium.By;
+
+public class WebTablesPage extends ElementsPage {
+
+    private By registrationAgeField= By.id("age");
+    private By submitButton = By.id("submit");
+
+    public void clickEdit(String email){
+        By edit = By.xpath("//div[text() ='"+ email +"']//following::span[@title='Edit']");
+        click(edit);
+    }
+
+    public void setAge(String age){
+        set(registrationAgeField,age);
+    }
+
+    public void clickSubmitButton(){
+        click(submitButton);
+    }
+
 }
---
+++ src/test/java/part3_4/com/demoqa/tests/part3/elements/WebTableTest.java
@@ -0,0 +1,12 @@
+package part3_4.com.demoqa.tests.part3.elements;
+
+import org.testng.annotations.Test;
+import part3_4.com.demoqa.base.BaseTest;
+
+public class WebTableTest extends BaseTest {
+    @Test
+
+
+
+
+}
